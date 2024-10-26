fada969261981686ac42e0f7a56a054e6dd8bbfc
Tap-Kim <rlaxogus0517@gmail.com>
2024-10-27 00:34:51 +0900

    feat: pnpm initial setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: './apps/petite-popper',
+  publicDir: 'apps/petite-popper/public', // public 디렉터리 경로 설정
+});
